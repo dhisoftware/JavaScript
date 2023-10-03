8e3a4f82873f907d2281466e1e31426fc5b1b740
matiasolivera <matiasolivera.dhi@gmail.com>
2023-10-02 21:20:46 -0300

    Segunda entrega Funcion de Orden Superior

--- scripts/clases/OrdenDeCompraLinea.js
+++ scripts/clases/OrdenDeCompraLinea.js
@@ -3,8 +3,16 @@ class OrdenDeCompraLinea {
     this.descripcion = descripcion;
     this.importe = importe;
     this.cantidad = cantidad;
-    this.total = this.importe * this.cantidad;
+    this.total = this.calcularTotalLinea(this.calcularTotalPorDefecto);
   }
+
+  calcularTotalLinea = (calculo) => {
+    return calculo(this.importe, this.cantidad);
+  }
+
+  calcularTotalPorDefecto = (importe, cantidad) => {
+    return importe * cantidad;
+  };
 
   toString = () => {
     return `${this.descripcion}, ${this.importe} x ${this.cantidad} Total: ${this.total}`;
